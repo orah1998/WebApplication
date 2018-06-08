3752e4da4889c69411f4883957095245e09703f3
or aharoni <orah1998@gmail.com>
2018-06-08 13:28:51 +0300

    updated photo buttons

---
+++ WebApplication2/Controllers/PhotoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class PhotoController : Controller
+    {
+        static DeletePhotoNeeds needs = new DeletePhotoNeeds("mip");
+
+
+        // GET: Photo
+        public ActionResult Edit(string mip)
+        {
+            needs = new DeletePhotoNeeds(mip);
+            return View(needs);
+        }
+
+
+
+
+
+
+
+    }
+}
